1565dc21b9f34170873644a526bd89bada9cd243
mocchan <hcmnorgir0808@yahoo.co.jp>
2023-02-04 19:16:16 +0900

    コード修正

--- SampleAVFoundation/ViewController.swift
+++ SampleAVFoundation/ViewController.swift
@@ -9,140 +9,76 @@ import UIKit
 import AVFoundation
 
 class ViewController: UIViewController {
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        .landscapeRight
+    }
 
+    override var preferredInterfaceOrientationForPresentation: UIInterfaceOrientation {
+        .landscapeRight
+    }
+
+    @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var previewView: UIView!
-
-    // inputsとoutputsの仲介役を行なっているのがAVCaptureSession
-    private var captureSession = AVCaptureSession()
-
-    // inputs: カメラやマイクなどデバイスからデータを取得する方法
-    private var captureDeviceInput: AVCaptureDeviceInput!
-    private var currentDevice: AVCaptureDevice?
-
-    // outputs: 出力形式。入力されたデータをどのように出力するか
-    private let photoOutput = AVCapturePhotoOutput()
-    private var cameraPreviewLayer: AVCaptureVideoPreviewLayer!
+    var captureSession: AVCaptureSession?
+    var stillImageOutput: AVCapturePhotoOutput?
+    var videoPreviewLayer: AVCaptureVideoPreviewLayer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        setupDevice()
-        setupInputOutput()
-        setupPreviewLayer()
-        captureSession.startRunning()
-    }
 
-    func setupDevice() {
-        // デバイスの設定
-        let deviceDiscoverySession = AVCaptureDevice.DiscoverySession(
-            deviceTypes: [.builtInWideAngleCamera],
-            mediaType: .video,
-            position: .back
-        )
-        self.currentDevice = deviceDiscoverySession.devices.first
-    }
+        captureSession = AVCaptureSession()
+        captureSession?.sessionPreset = .high
 
-    func setupInputOutput() {
+        guard let backCamera = AVCaptureDevice.default(for: AVMediaType.video) else {
+            print("Unable to access back camera!")
+            return
+        }
+
         do {
-            // 入力(カメラ)と出力(画像)の設定
-            guard let currentDevice = self.currentDevice else { return }
-            self.captureDeviceInput = try AVCaptureDeviceInput(device: currentDevice)
-            self.captureSession.addInput(self.captureDeviceInput)
+            let input = try AVCaptureDeviceInput(device: backCamera)
+            stillImageOutput = AVCapturePhotoOutput()
 
-            // 出力ファイルのフォーマットをjpegに指定する
-            self.photoOutput.setPreparedPhotoSettingsArray(
-                [AVCapturePhotoSettings(
-                    format: [AVVideoCodecKey: AVVideoCodecType.jpeg]
-                )]
-            )
-
-            //
-            self.captureSession.addOutput(self.photoOutput)
-        } catch {
-            fatalError("\(error)")
+            if (captureSession?.canAddInput(input) == true && captureSession?.canAddOutput(stillImageOutput!) == true) {
+                captureSession?.addInput(input)
+                captureSession?.addOutput(stillImageOutput!)
+                setupLivePreview()
+            }
+        }
+        catch let error  {
+            print("Error Unable to initialize back camera:  \(error.localizedDescription)")
         }
     }
 
-    func setupPreviewLayer() {
-        // previewの設定
-        self.cameraPreviewLayer = AVCaptureVideoPreviewLayer(session: self.captureSession)
-        self.captureSession.sessionPreset = .photo
-        // videoGravity: カメラのプレビューをどこまで広げるか
-        self.cameraPreviewLayer.videoGravity = .resizeAspectFill
-//        self.cameraPreviewLayer.connection?.videoOrientation = .portrait
-        DispatchQueue.main.async {
-            self.cameraPreviewLayer.frame = self.previewView.bounds
-            self.previewView.layer.addSublayer(self.cameraPreviewLayer)
+    func setupLivePreview() {
+        videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession!)
+        videoPreviewLayer?.videoGravity = .resizeAspect
+        videoPreviewLayer?.connection?.videoOrientation = .landscapeRight
+        previewView.layer.addSublayer(videoPreviewLayer!)
+        DispatchQueue.global().async { [weak self] in
+            self?.captureSession?.startRunning()
         }
     }
 
-    @IBAction func tappedTakePicture(_ sender: Any) {
-        // 写真撮影の設定周り
-        let settings = AVCapturePhotoSettings()
-        settings.flashMode = .auto
-        self.photoOutput.capturePhoto(with: settings, delegate: self)
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        videoPreviewLayer?.frame = previewView.bounds
     }
 
-    @IBAction func changeOutputPosition(_ sender: Any) {
-        // 一旦previewを止める
-        self.captureSession.stopRunning()
-
-        // inputs, outputsをリセット
-        self.captureSession.inputs.forEach { input in
-            self.captureSession.removeInput(input)
-        }
-        self.captureSession.outputs.forEach { output in
-            self.captureSession.removeOutput(output)
-        }
-
-        // prepare new camera preview
-        let newCameraPosition: AVCaptureDevice.Position = self.currentDevice?.position == .front ? .back : .front
-        self.setupCaptureSession(withPosition: newCameraPosition)
-
-        let newVideoLayer: AVCaptureVideoPreviewLayer = AVCaptureVideoPreviewLayer(session: self.captureSession)
-        newVideoLayer.frame = previewView.bounds
-        newVideoLayer.videoGravity = .resizeAspectFill
-
-        // horizontal flip
-//        UIView.transition(with: self.view, duration: 0.3, options: [.transitionFlipFromLeft], animations: nil, completion: { _ in
-            // replace camera preview with new one
-        DispatchQueue.main.async {
-            self.setupPreviewLayer()
-            self.captureSession.startRunning()
-        }
-//        })
-    }
-
-    func setupCaptureSession(withPosition cameraPosition: AVCaptureDevice.Position) {
-        self.currentDevice = AVCaptureDevice.default(AVCaptureDevice.DeviceType.builtInWideAngleCamera, for: AVMediaType.video, position: cameraPosition)
-        self.captureSession = AVCaptureSession()
-
-        // add video input to a capture session
-        let videoInput = try! AVCaptureDeviceInput(device: self.currentDevice!)
-        self.captureSession.addInput(videoInput)
-
-        // add capture output
-        let captureOutput: AVCaptureMovieFileOutput = AVCaptureMovieFileOutput()
-        self.captureSession.addOutput(captureOutput)
-
-        self.captureSession.startRunning()
+    @IBAction func takePhoto(_ sender: UIButton) {
+        let settings = AVCapturePhotoSettings(format: [AVVideoCodecKey: AVVideoCodecType.jpeg])
+        stillImageOutput?.capturePhoto(with: settings, delegate: self)
     }
 }
 
 extension ViewController: AVCapturePhotoCaptureDelegate {
-    // 写真が撮影される直前に呼ばれる
-    func photoOutput(_ output: AVCapturePhotoOutput, willCapturePhotoFor resolvedSettings: AVCaptureResolvedPhotoSettings) {
-        AudioServicesDisposeSystemSoundID(1036)
-    }
-
-    // 写真のProcessingが終わった後
     func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
-        // data型に変更
-        print(output)
-        let imageData = photo.fileDataRepresentation()
-        // 型変換
-        guard let photo = UIImage(data: imageData!) else { return }
-        //写真 ライブラリに画像を保存
-        UIImageWriteToSavedPhotosAlbum(photo, self, nil, nil)
+        guard let imageData = photo.fileDataRepresentation() else { return }
+
+        let originalImage = UIImage(data: imageData)
+        let rect = CGRect(x: 0, y: 0, width: 300, height: 300)
+        let cgImage = originalImage?.cgImage?.cropping(to: rect)
+        let croppedImage = UIImage(cgImage: cgImage!)
+
+        imageView.image = croppedImage
     }
 }
